5ec60fda7acad61dfe55deda9d003d12b7394411
vladimircuc <vladi3030@gmail.com>
2025-10-09 19:36:36 -0400

    both merges toegtehr

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,6 @@ from fastapi.middleware.cors import CORSMiddleware
 # ------------------------------
 app = FastAPI(title="QUORRA LLM API")
 
-app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],  # or ["*"] for testing
